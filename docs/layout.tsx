50dddf6d07824904f9e8bf82d92f2b5f092195eb
Richard Miguel Irala <richardirala47@hotmail.com>
2024-07-08 19:16:51 -0300

    Added a content search reset when the page is reloaded

--- docs/layout.tsx
+++ docs/layout.tsx
@@ -1,7 +1,18 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import TopNavButton from "./components/TopNavButton";
+import FullScreenLoading from "./components/FullScreenLoading";
 
 const Layout = ({ children }: { children: ReactElement | string }) => {
+  const [restartedSearch, setRestartedSearch] = useState(false);
+  useEffect(() => {
+    if (localStorage) {
+      localStorage.removeItem("filterText");
+      setRestartedSearch(true);
+    }
+  }, []);
+
+  if (!restartedSearch) return <FullScreenLoading />;
+
   return (
     <div>
       <TopNavButton />
