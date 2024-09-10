ef68f1091c13c548c40f361ada4a64a967f93099
Richard Miguel Irala <richardirala47@hotmail.com>
2024-09-10 14:53:27 -0300

    Fixed start building css issue

--- docs/components/TopNavButton.tsx
+++ docs/components/TopNavButton.tsx
@@ -1,16 +1,35 @@
-import type { ReactElement } from 'react'
+import { useState, useEffect, ReactElement } from 'react';
 
 const TopNavButton = (): ReactElement => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 5) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <button
-      className="hover-fade font-bold text-white max-w-max h-min text-center rounded-full bg-gradient-to-r from-[#4411E1] to-[#7537F9] px-[16px] py-[8px] text-[14px] leading-[20px] ml-[8px] top-nav-button_position"
+      className={`hover-fade font-bold text-white max-w-max h-min text-center rounded-full bg-gradient-to-r from-[#4411E1] to-[#7537F9] px-[16px] py-[8px] text-[14px] leading-[20px] ml-[8px] 
+        ${scrolled && window.innerWidth < 1080 ? `top-nav-button_position transform translate-x-[-140px] translate-y-[-65%]` : 'top-nav-button_position'}`}
       onClick={() => {
-        window.open('https://sequence.build/', '_blank')
+        window.open('https://sequence.build/', '_blank', 'noopener,noreferrer');
       }}
     >
       Start building
     </button>
-  )
-}
+  );
+};
 
-export default TopNavButton
+export default TopNavButton;
