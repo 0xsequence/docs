076ebe680a11513061cabfb1b48186d1b106c147
Richard Miguel Irala <richardirala47@hotmail.com>
2024-09-18 05:01:45 -0300

    Create a component for tabbed content in the UI. Applied to the Primary Sales documentation

---
+++ docs/components/TabbedContent/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import './style.css';
+
+interface TabbedContentProps {
+  labels: string[];
+}
+
+function hideInactiveTabs(document: Document, activeTab: number): void {
+  const tabsContent = document.querySelectorAll('.tabbed-content__content');
+
+  tabsContent.forEach((content, index) => {
+    if (content instanceof HTMLElement) {
+      content.classList.add('hidden');
+      if (index === activeTab) {
+        content.classList.remove('hidden');
+      }
+    }
+  });
+}
+
+const TabbedContent = ({ labels }: TabbedContentProps) => {
+  const [activeTab, setActiveTab] = useState<number>(0);
+
+  const handleTabChange = (index: number) => {
+    setActiveTab(index);
+  };
+
+  useEffect(() => {
+    hideInactiveTabs(document, activeTab);
+  }, [activeTab]);
+
+  return (
+    <div>
+      <div className="tabbed-content__menu">
+        {labels.map((label, index) => (
+          <button
+            key={label}
+            className={`tabbed-content__menu-button ${index === activeTab ? 'tabbed-content__menu-button--active' : ''}`}
+            onClick={() => handleTabChange(index)}
+          >
+            {label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TabbedContent;
