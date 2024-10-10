9a7fd8cb34490a5f805df405266c27026ca726f8
James Lawton <crossoveranx@gmail.com>
2024-10-10 18:31:23 +0100

    Revert "added animation (#345)" (#346)
    This reverts commit 1025845aab4851c9ec97b19d5c57864157689ecf.

--- docs/components/Landing/GradientBackground.tsx
+++ docs/components/Landing/GradientBackground.tsx
@@ -12,20 +12,7 @@ export function GradientBackground({
     <div className={clsx('relative', className)}>
       <div className="absolute inset-0 z-0 flex items-center">
         <div className="w-full h-full max-h-[600px] dark:bg-gradient-dark bg-gradient-light" />
-
-        <div
-          className="clouds animate-clouds"
-          style={{ backgroundImage: 'url(/img/bot/clouds.svg)' }}
-          aria-label="Animated Clouds"
-        />
-
-        <div
-          className="clouds2 animate-clouds2"
-          style={{ backgroundImage: 'url(/img/bot/clouds2.svg)' }}
-          aria-label="Animated Clouds 2"
-        />
       </div>
-
       <div className="relative z-10">{children}</div>
     </div>
   )
