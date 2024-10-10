5c9b46e10240f58c88c21720bbb931b0f788c416
James Lawton <jlaw@horizon.io>
2024-10-10 18:17:41 +0100

    added animation

--- docs/components/Landing/GradientBackground.tsx
+++ docs/components/Landing/GradientBackground.tsx
@@ -11,21 +11,20 @@ export function GradientBackground({
   return (
     <div className={clsx('relative', className)}>
       <div className="absolute inset-0 z-0 flex items-center">
-      <div className="w-full h-full max-h-[600px] dark:bg-gradient-dark bg-gradient-light" />
+        <div className="w-full h-full max-h-[600px] dark:bg-gradient-dark bg-gradient-light" />
 
-      <div
-        className="clouds animate-clouds"
-        style={{ backgroundImage: 'url(/img/bot/clouds.svg)' }}
-        aria-label="Animated Clouds"
-      />
+        <div
+          className="clouds animate-clouds"
+          style={{ backgroundImage: 'url(/img/bot/clouds.svg)' }}
+          aria-label="Animated Clouds"
+        />
 
-      <div
-        className="clouds2 animate-clouds2"
-        style={{ backgroundImage: 'url(/img/bot/clouds2.svg)' }}
-        aria-label="Animated Clouds 2"
-      />
+        <div
+          className="clouds2 animate-clouds2"
+          style={{ backgroundImage: 'url(/img/bot/clouds2.svg)' }}
+          aria-label="Animated Clouds 2"
+        />
       </div>
-      
 
       <div className="relative z-10">{children}</div>
     </div>
