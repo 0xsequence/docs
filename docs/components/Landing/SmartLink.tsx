83a26e3d0beb6f743667259cd289c054c490ab09
VGabriel45 <vgabrielmarian21@gmail.com>
2025-01-31 14:15:09 +0200

    fixes for sequence kit docs

--- docs/components/Landing/SmartLink.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithoutRef } from 'react'
-
-export const SmartLink = ({ href, ...props }: PropsWithoutRef<JSX.IntrinsicElements['a']>) => {
-  if (href?.startsWith('/')) return <a href={href} {...props} />
-
-  return <a href={href} target="_blank" rel="noopener noreferrer" {...props} />
-}
