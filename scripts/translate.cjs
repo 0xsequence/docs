6e8869159598a26cb70caf8c7908a33a31e31a33
akgunberk <akgunberk@yahoo.com>
2025-02-03 13:52:42 +0300

    add translate script for only diffed files

--- scripts/translate.cjs
+++ scripts/translate.cjs
@@ -11,12 +11,13 @@ const EXCLUDED_TRANSLATION_PATH = ['docs/pages/ja', 'docs/pages/solutions/chainl
 const frenglish = new FrenglishSDK(FRENGLISH_API_KEY)
 
 const isVerbose = process.argv.includes('--v') || process.argv.includes('--verbose')
+const shouldTranslateAllFiles = process.argv.includes('--translate-all')
 
 function log(...args) {
   if (isVerbose) console.log(...args)
 }
 
-function getChangedFiles() {
+function getAllFiles() {
   log('Getting all files from:', ORIGIN_LANGUAGE_DIR)
 
   try {
@@ -29,6 +30,23 @@ function getChangedFiles() {
     return files
   } catch (error) {
     console.error('Error fetching files:', error.message)
+    return []
+  }
+}
+
+function getChangedFiles() {
+  log('Getting changed files from git status in:', ORIGIN_LANGUAGE_DIR)
+
+  try {
+    const grepExcludedPaths = EXCLUDED_TRANSLATION_PATH.map((path) => `:!${path}`).join(' ')
+    const gitCommand = `git status --porcelain "${ORIGIN_LANGUAGE_DIR}" ${grepExcludedPaths} | awk '{print $2}'`
+
+    const files = execSync(gitCommand, { encoding: 'utf8' }).trim().split('\n').filter(Boolean) // Remove empty strings
+
+    log('Files to translate:', files)
+    return files
+  } catch (error) {
+    console.error('Error fetching changed files:', error.message)
     return []
   }
 }
@@ -85,10 +103,10 @@ async function writeTranslatedFiles(translationContent, filesToTranslate) {
 }
 
 async function main() {
-  const filesToTranslate = getChangedFiles()
+  const filesToTranslate = shouldTranslateAllFiles ? getAllFiles() : getChangedFiles()
   if (filesToTranslate.length === 0) {
     console.warn('No files to translate')
-    return
+    process.exit(1)
   }
 
   const translationContent = await translateFiles(filesToTranslate)
@@ -96,66 +114,5 @@ async function main() {
     await writeTranslatedFiles(translationContent, filesToTranslate)
   }
 }
-
-// async function main() {
-//   try {
-//     const filesToTranslate = getChangedFiles();
-//
-//     if (filesToTranslate.length === 0) {
-//       log("No files to translate");
-//       return;
-//     }
-//
-//     log(filesToTranslate);
-//
-//     const fileContents = await Promise.all(
-//       filesToTranslate.map(async (file) => {
-//         const content = await fs.readFile(file, "utf-8");
-//         log("content AAA", content);
-//         return { fileId: path.basename(file), content };
-//       })
-//     );
-//
-//     const filenames = fileContents.map((file) => file.fileId);
-//     const contents = fileContents.map((file) => file.content);
-//
-//     // Adjust the translate call based on the SDK's expected parameters
-//     // log("before translate contents", contents);
-//     const translation = await frenglish.translate(contents, false, filenames);
-//     log(`Translation requested with ID: ${translation.translationId} `);
-//
-//     for (const languageData of translation.content) {
-//       const language = languageData.language;
-//       const translatedFiles = languageData.files;
-//       // log("translation.content ", translation.content);
-//
-//       for (const translatedFile of translatedFiles) {
-//         const originalFile = filesToTranslate.find((file) => path.basename(file) === translatedFile.fileId);
-//         if (originalFile) {
-//           const translatedFilePath = originalFile.replace(
-//             ORIGIN_LANGUAGE_DIR,
-//             path.join(ORIGIN_LANGUAGE_DIR, language)
-//           );
-//           await fs.mkdir(path.dirname(translatedFilePath), { recursive: true });
-//           if (translatedFile.content.length > 0) {
-//             await fs.writeFile(translatedFilePath, translatedFile.content, "utf8");
-//             log(`Translated file written: ${translatedFilePath} `);
-//           } else {
-//             console.warn(`Empty content for file: ${translatedFile.fileId}.Skipping.`);
-//           }
-//         } else {
-//           console.warn(`Original file not found for translated file: ${translatedFile.fileId} `);
-//         }
-//       }
-//     }
-//   } catch (error) {
-//     console.error("Error during translation process:", JSON.stringify(error));
-//     if (error.response) {
-//       console.error("Response status:", error.response.status);
-//       console.error("Response data:", await error.response.text());
-//     }
-//     process.exit(1);
-//   }
-// }
 
 main()
