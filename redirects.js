80d1a9b0d604539113a4358a90f4f691ccd3ebc2
James Lawton <jlaw@horizon.io>
2025-03-14 09:40:35 +0000

    updated function name

--- redirects.js
+++ redirects.js
@@ -1,7 +1,7 @@
 // redirects.js - Custom JavaScript for handling API redirects
 // This file will be automatically included in every documentation page
 
-(function() {
+(function sequenceApiRedirects() {
   // Map of old API routes to new routes
   const redirects = {
     "/api/transactions/overview": "/api-references/transactions/overview",
